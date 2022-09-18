680c6b5ee615a6e78060dc133db174d0f6f35213
Sergey Melnikov <melnikov.s220@gmail.com>
2022-09-18 11:19:46 -0400

    introduced internal computed node

--- src/api/computed.ts
+++ src/api/computed.ts
@@ -2,11 +2,14 @@ import ComputedNode from "../core/nodes/computed";
 import { getOpts, propertyType } from "../observables/utils/configuration";
 import { getCtorConfiguration } from "../observables/utils/lookup";
 import { isPropertyKey } from "../utils";
-import { resolveGraph, Graph, setNode } from "./graph";
+import { resolveGraph, Graph, setNode, getNode } from "./graph";
 
-export type Computed<T> = (() => T) & {
+export type Computed<T> = () => T;
+
+export type InternalComputedNode<T> = {
 	clear: () => void;
 	equals: (value: T) => boolean;
+	get: () => T;
 	isDirty: () => boolean;
 	isKeepAlive: () => boolean;
 	setKeepAlive: (keepAlive: boolean) => void;
@@ -18,6 +21,11 @@ export type ComputedOptions<T> = {
 	keepAlive?: boolean;
 	context?: unknown;
 };
+
+const internalComputedNodes = new WeakMap<
+	Computed<unknown>,
+	InternalComputedNode<unknown>
+>();
 
 function computed<T>(fn: () => T, opts?: ComputedOptions<T>): Computed<T>;
 
@@ -48,18 +56,39 @@ function computed<T>(...args: unknown[]): unknown {
 			opts?.context
 		);
 
-		const computed = Object.assign(computedNode.get.bind(computedNode), {
-			clear: computedNode.clear.bind(computedNode),
-			equals: computedNode.equals.bind(computedNode),
-			isDirty: computedNode.isDirty.bind(computedNode),
-			isKeepAlive: computedNode.isKeepAlive.bind(computedNode),
-			setKeepAlive: computedNode.setKeepAlive.bind(computedNode),
-		});
+		const computed = computedNode.get.bind(computedNode);
 
 		setNode(computed, computedNode);
 
 		return computed;
 	}
+}
+
+export function getInternalComputedNode<T>(
+	fn: () => T
+): InternalComputedNode<T> {
+	let internalNode = internalComputedNodes.get(fn);
+
+	if (!internalNode) {
+		const computedNode = getNode(fn);
+
+		if (!computedNode) {
+			throw new Error("getInternalComputedNode: parameter is not a computed!");
+		}
+
+		internalNode = {
+			get: computedNode.get.bind(computedNode),
+			clear: computedNode.clear.bind(computedNode),
+			equals: computedNode.equals.bind(computedNode),
+			isDirty: computedNode.isDirty.bind(computedNode),
+			isKeepAlive: computedNode.isKeepAlive.bind(computedNode),
+			setKeepAlive: computedNode.setKeepAlive.bind(computedNode),
+		};
+
+		internalComputedNodes.set(fn, internalNode);
+	}
+
+	return internalNode as InternalComputedNode<T>;
 }
 
 Object.assign(computed, propertyType.computed);
